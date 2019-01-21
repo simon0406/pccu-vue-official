ab379c1bde14367f617c730cda514ec819ab0d77
Chuian <chuiantw1212@gmail.com>
2019-01-21 21:55:35 +0800

    add 文案

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,36 @@
     <div class="container">
       <div class="row">
         <Card>
+          <h4>前端</h4>
+          <ul>
+            <li>前端技能：Javascript, HTML, SCSS</li>
+            <li>前端框架：Vue.js (Vuex), Bootstrap</li>
+            <li>管理工具：Webpack, Gulp</li>
+            <li>連線模組：axios, socket.io</li>
+          </ul>
+        </Card>
+        <Card>
+          <h4>後端</h4>
+          <ul>
+            <li>後端技能：Node.js (以JsDoc+Typescript強化)</li>
+            <li>後端框架：Express</li>
+            <li>資料庫：MS SQL Server</li>
+            <li>CICD：Docker (Portainer)</li>
+          </ul>
+        </Card>
+        <Card>
+          <h4>其他</h4>
+          <ul>
+            <li>撰碼風格：MVC, 物件導向, 扁平化, 原生優先, Async First</li>
+            <li>開發環境：VS Code</li>
+            <li>版本控制：GitKraken, GitHub</li>
+            <li>作業系統：Mac, Windows</li>
+            <li>領域知識：Arbitrage, Blockchain</li>
+          </ul>
+        </Card>
+      </div>
+      <div class="row">
+        <Card>
           <img src="@/assets/lobster.jpg">
           <h3>龍蝦</h3>
           <p>說明文字</p>
@@ -19,7 +49,6 @@
           <p>說明文字</p>
         </Card>
       </div>
-      <Footer />
       <ListItem />
     </div>
   </div>
@@ -31,7 +60,6 @@ export default {
   components: {
     Navigation: () => import("./components/Navigation.vue"),
     Card: () => import("./components/Card.vue"),
-    Footer: () => import("./components/Footer.vue"),
     ListItem: () => import("./components/ListItem.vue")
   }
 };
@@ -56,5 +84,10 @@ header {
 
 section {
   padding: 150px 0;
+}
+
+.container {
+  min-height: 100vh;
+  position: relative;
 }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,22 +1,30 @@
 <template>
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">{{title}}</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarResponsive"
-        aria-controls="navbarResponsive"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <!-- <li class="nav-item">
+  <nav
+    class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
+    id="mainNav"
+  >
+    <a
+      class="navbar-brand js-scroll-trigger"
+      href="#page-top"
+    >{{title}}</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarResponsive"
+      aria-controls="navbarResponsive"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      class="collapse navbar-collapse"
+      id="navbarResponsive"
+    >
+      <ul class="navbar-nav ml-auto">
+        <!-- <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#lesson1">Lesson1</a>
           </li>
           <li class="nav-item">
@@ -28,8 +36,7 @@
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#lesson4">Lesson 4</a>
           </li> -->
-        </ul>
-      </div>
+      </ul>
     </div>
   </nav>
 </template>
