dd7d802fc98412bf1dd2a67b7ce02e54c126a525
Chuian <chuiantw1212@gmail.com>
2019-05-20 20:16:31 +0800

    modify shell

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,6 @@
-#!/usr/bin/env sh
-
-# 当发生错误时中止脚本
-set -e
-
-# 构建
-npm run build
-
-# cd 到构建输出的目录下 
-cd dist
-
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Chuiantw1212/pccu-vue-official.git master:gh-pages
-
-cd -
+npm run build         
+git branch gh-pages     #创建gh-pages分支
+git checkout gh-pages   #切换到gh-pages分支
+git add -f dist         #强制把dist文件夹提交到github
+git subtree push --prefix dist origin gh-pages  #把dist文件夹单独部署到 gh-pages 分支
+git checkout master
