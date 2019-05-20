ffaed15e7e5b4e96532a5ae649c325a3a45e86c4
Chuian <chuiantw1212@gmail.com>
2019-05-20 20:17:58 +0800

    update code

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ npm run build
 git branch gh-pages     #创建gh-pages分支
 git checkout gh-pages   #切换到gh-pages分支
 git add -f dist         #强制把dist文件夹提交到github
+git commit -m "update dist"
 git subtree push --prefix dist origin gh-pages  #把dist文件夹单独部署到 gh-pages 分支
 git checkout master
