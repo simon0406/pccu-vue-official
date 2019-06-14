79c628f1e0e899e272a12f3867583ace6d0f002f
Chuian <chuiantw1212@gmail.com>
2019-06-14 22:20:01 +0800

    done

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 npm run build         
 git add -f dist         #强制把dist文件夹提交到github
 git commit -m "update dist"
-git subtree push --prefix dist origin gh-pages  #把dist文件夹单独部署到 gh-pages 分支
+git push origin `git subtree split --prefix dist master`:gh-pages --force
